f39d2147f76435e66b9920191910d99424b7a9a1
sandeepsingh2002 <lodhisandeep2002@gmail.com>
2026-03-07 00:24:02 +0530

    fix: correct model import casing for Linux

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Transaction = require('../models/transaction');
+const Transaction = require('../models/Transaction');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 // All routes protected by authMiddleware
